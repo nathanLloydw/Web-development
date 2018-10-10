7365acd0cd75336d7f2cded04c24ddaff9dcbba6
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2018-10-10 11:11:06 +0100

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,20 @@
- function changeText() {
-    document.getElementById("test").innerHTML = "Paragraph changed... in external file";
+//Apply emoji to #cursor::after 
+function applyEmoji(e){
+  var val = $(e).val();
+  $("#cursor").attr("data-after",val);
 }
+
+//Track location of cursor
+$("body").mousemove(function(cursor){
+	var xLeft = cursor.pageX;
+	var yTop = cursor.pageY;
+	$("#cursor").css("left",xLeft).css("top",yTop);
+});
+
+//Change emoji on dropdown selection
+$("select").on('change', function(){
+	applyEmoji(this);
+});
+
+//On page load, use the "selected" emoji - Ghostman
+applyEmoji("option[selected]");
