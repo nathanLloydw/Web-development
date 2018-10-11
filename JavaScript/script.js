e8f7613a8a6ca30659796d478b7ab80ca58cb70f
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2018-10-11 09:56:33 +0100

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,34 @@
 
+var n = 16;
+var name = "bob";
+var bool = true;
+var booksOfTheBible = ['john','genesis','daniel','exodus'];
+var god = {name: "God", age: 2018, magic: true};
+
+n = n + 1;
+
+document.write('hello'+' world');
+
+n = ((n - 2) * 3) / 4;
+
+if(n === 12)
+{
+ document.write("true"); 
+}
+else
+{
+  document.write("false");
+}
+
+function doSomethingDifficult(){
+  return “I have done the impossible!”;
+}
+
+var difficultStuff = doSomethingDifficult();
+
+document.write(difficultStuff);
+
+document.querySelector(‘body’).onClick = function() {
+  // do something e.g alert
+  alert(“Click click!”);
+};
