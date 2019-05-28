02be1401c58fd45297436d4efe8c668b7f74c3f3
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2019-05-28 12:59:16 +0100

    Update TicketSystem.js

--- JavaScript/TicketSystem.js
+++ JavaScript/TicketSystem.js
@@ -1,15 +1,11 @@
 window.onload = function(){ 
     // your code 
 
-function click() 
-{
-     console.log("click!");
-     var form = document.getElementById("form");
-     form.style.display = "none";
-}
-
-document.getElementById("button").onclick = click;
-click();
-
+    function click() 
+    {
+        console.log("click!");
+        var form = document.getElementById("form");
+        form.style.display = "none";
+    }
     
 };
