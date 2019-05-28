58323449a9fd1927f5a28365f181ef1e1d6d3117
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2019-05-28 13:29:22 +0100

    Update TicketSystem.js

--- JavaScript/TicketSystem.js
+++ JavaScript/TicketSystem.js
@@ -18,7 +18,7 @@ window.onload = function(){
         header.innerHTML = "ticket number";
         ticket.innerHTML = randomTicket;
         
-        waitTime.style.innerHTML = "your wait time will be roughly "+Math.floor((Math.random()*10))*5+" and "+Math.floor((Math.random()*10))*5+" hours";
+        waitTime.innerHTML = "your wait time will be roughly "+Math.floor((Math.random()*10))*5+" and "+Math.floor((Math.random()*10))*5+" hours";
     }
     
     document.getElementById("button").onclick = click;
