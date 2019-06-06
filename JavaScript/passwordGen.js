7ae43d6cb12631a5d90359d762870fffc31c045a
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2019-06-06 14:53:10 +0100

    Update passwordGen.js

--- JavaScript/passwordGen.js
+++ JavaScript/passwordGen.js
@@ -1,3 +1,15 @@
+function sleep(milliseconds) 
+{
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) 
+    {
+        if ((new Date().getTime() - start) > milliseconds)
+        {
+            break;
+        }
+    }
+}
+
 var mouse = 
 {
     x: undefined,
@@ -24,6 +36,14 @@ window.addEventListener('mouseup',function(event)
 
 });
 
-
-
 scroller = document.getElementById("scroller_item");
+ var scroller_x = scroller.offsetLeft;
+ var scroller_y = scroller.offsetTop;
+
+while(true)
+{
+   sleep(10)
+   console.log("mouse: ",mouse.x," ",mouse.y);
+   console.log("scroller: ",scroller_x," ",scroller_y);
+}
+
