729a3aedcaf7984cf38da9ab3fb409a0ae914eff
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2019-06-11 11:54:06 +0100

    Update passwordGen.js

--- JavaScript/passwordGen.js
+++ JavaScript/passwordGen.js
@@ -1,6 +1,14 @@
 function update()
 {
     console.log('updating...');    
+    
+    var slider_length = document.getElementById('slider_length');
+    var slider_numbers = document.getElementById('slider_numbers');
+    var slider_symbols = document.getElementById('slider_symbols');
+    
+    password_length.innerHTML = slider_length.value;
+    password_numbers.innerHTML = slider_numbers.value
+    password_symbols.innerHTML = slider_symbols.value;
 }
 
 window.onload = function()
@@ -9,10 +17,6 @@ window.onload = function()
      var password_numbers = document.getElementById('password_numbers');
      var password_symbols = document.getElementById('password_symbols');
      
-     //var slider_length = document.getElementById('slider_length');
-     //var slider_numbers = document.getElementById('slider_numbers');
-     //var slider_symbols = document.getElementById('slider_symbols');
-
      password_length.innerHTML = "12";
      password_numbers.innerHTML = "13";
      password_symbols.innerHTML = "14";
