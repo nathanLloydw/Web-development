a2de5bc267c9f54548d156cbb5854d7915aad2b5
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2019-06-11 09:03:49 +0100

    Update passwordGen.js

--- JavaScript/passwordGen.js
+++ JavaScript/passwordGen.js
@@ -1,1 +1,9 @@
 
+var password_length = document.getElementById('password_length');
+var password_numbers = document.getElementById('password_numbers');
+var password_symbols = document.getElementById('password_symbols');
+
+while(true)
+{
+     password_length.innerHTML = "12";
+}
