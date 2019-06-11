a17a03882d809f114dc47c3845bc6518aa91e03a
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2019-06-11 12:46:08 +0100

    Update passwordGen.js

--- JavaScript/passwordGen.js
+++ JavaScript/passwordGen.js
@@ -9,6 +9,23 @@ function update()
     password_length.innerHTML = slider_length.value;
     password_numbers.innerHTML = slider_numbers.value
     password_symbols.innerHTML = slider_symbols.value;
+    
+    switch(slider_length.value)
+    {
+     case 6: password.innderHTML = words6[Math.floor(Math.random() * myArray.length)];
+     case 7: password.innderHTML = words7[Math.floor(Math.random() * myArray.length)];
+     case 8: password.innderHTML = words8[Math.floor(Math.random() * myArray.length)];
+     case 9: password.innderHTML = words9[Math.floor(Math.random() * myArray.length)];
+     case 10: password.innderHTML = "test";
+     case 11: password.innderHTML = "test";
+     case 12: password.innderHTML = "test";
+     case 13: password.innderHTML = "test";
+     case 14: password.innderHTML = "test";
+     case 15: password.innderHTML = "test";
+     case 16: password.innderHTML = "test";
+     case 17: password.innderHTML = "test";
+     case 18: password.innderHTML = "test";
+    }
 }
 
 window.onload = function()
@@ -16,14 +33,16 @@ window.onload = function()
      words1=['I','A'];
      words2=['to','am','of','in','it','is','be','as','at','so','we','be'];
      words3=['are','can','sad','bat','the','for','cat','dog','you','her','him','was','man'];
-     words4=['that','will','come','dick','hate','like','from','look','said','were','many','each','long','twat'];
+     words4=['that','will','come','dick','hate','like','from','look','said','were','many','each','long','twat','fuck','yeah'];
      words5=['bitch','twats','happy','hello','laugh','learn','drive','drove','broke','spoke','eager','guide','alive'];
      words6=['purple','orange','family','people','Africa','monkey','Monday','office','sister','Austin','turtle','pirate','secret'];
-     words7=['','','','','','','','','','','','',''];
-     words8=['','','','','','','','','','','','',''];
-     words9=['','','','','','','','','','','','',''];
+     words7=['perfect','pumpkin','America','freedom','hundred','silence','someone','science','revenge','musical','dancing','penguin','sixteen'];
+     words8=['Thursday','darkness','birthday','mountain','feminine','calendar','bullying','marriage','American','strength','champion','football','treasure'];
+     words9=['invisible','knowledge','community','irregular','secretary','celebrate','Halloween','Australia','pineapple','crocodile','pollution','ambulance','Christian'];
     
      var password_length = document.getElementById('password_length');
      var password_numbers = document.getElementById('password_numbers');
      var password_symbols = document.getElementById('password_symbols');
+    
+    var password = document.getElementById('password');
 }
