8c1da65d48cf63b62589d054a1f7ed2ee3ffbc0d
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2019-06-21 10:20:00 +0100

    Update game01.js

--- JavaScript/game01.js
+++ JavaScript/game01.js
@@ -1,1 +1,11 @@
+var context;
 
+function init()
+{
+    context = myCanvas.getContext('2d');
+    context.beginPath();
+    context.fillStyle="#0000ff";
+    // Draws a circle of radius 20 at the coordinates 100,100 on the canvas
+    context.arc(100,100,20,0,Math.PI*2,true); context.closePath();
+    context.fill();
+}
