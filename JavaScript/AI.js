8cc983e93144ca7818e6611d3d66d9a0e7b7b7e6
nathan <nathan.w.newport@gmail.com>
2019-07-04 15:40:07 +0100

    perfected the ratio for best land generation

--- JavaScript/AI.js
+++ JavaScript/AI.js
@@ -3,31 +3,33 @@ class AI
     constructor()
     {
         this.size = 35;
-        this.x = 100;
-        this.y = 100;
+        this.x = 500;
+        this.y = 500;
+     
     }
 
     move()
     {
         var num = int(random(0, 4));
+
         if(num == 0)
         {
-            this.x++;
+           this.x++;
         }
         else if(num == 1)
         {
-            this.x--;
+           this.x--;
         }
         else if(num == 2)
         {
-            this.y++;
+           this.y++;
         }
         else if(num == 3)
         {
-            this.y--;
-        }
-        console.log(num);
+           this.y--;
+        } 
     }
+    
     
     show()
     {
--- JavaScript/mapGeneration.js
+++ JavaScript/mapGeneration.js
@@ -1,31 +1,57 @@
-let tileSize = 5;
-let scl = 0.04;
+let tileSize = 2;
+let scl = 0.005; 
 
 function getNoiseColor(x,y)
 {
     noiseNum = noise(x*scl,y*scl);
-    
-    
-        if(noiseNum < 0.1){return color(0,71,186);} 
-        else if (noiseNum < 0.2){return color(4,129,196);} 
-        else if (noiseNum < 0.3){ return color(55,182,250);}
-        else if (noiseNum < 0.4){ return color(219,204,107);}
-        else if (noiseNum < 0.5){ return color(161,245,100);}
-        else if (noiseNum < 0.6){ return color(64,143,7);}
-        else if (noiseNum < 0.7){ return color(112,73,13);}
-        else if (noiseNum < 0.8){ return color(97,91,83);}
-        else if (noiseNum < 0.9){ return color(143,140,136);}
-        else if (noiseNum < 1.0){ return color(230,228,555);}
-        console.log(noiseNum);
-        return 3;
-    
+
+    if(noiseNum < 0.25){return color(16,93,134);}
+    else if(noiseNum < 0.27){return color(14,99,145);} 
+    else if(noiseNum < 0.28){return color(17,106,154);} 
+    else if(noiseNum < 0.29){return color(17,116,168);} 
+    else if (noiseNum < 0.31){return color(28,117,164);}
+    else if (noiseNum < 0.30){return color(24,121,173);}
+    else if (noiseNum < 0.315){return color(29,125,176);}
+    else if (noiseNum < 0.32){return color(23,138,200);}
+    else if (noiseNum < 0.325){return color(23,159,200);}
+    else if (noiseNum < 0.33){return color(19,173,219);}
+    else if (noiseNum < 0.335){return color(10,185,236);}
+    else if (noiseNum < 0.34){return color(25,203,255);}
+    else if (noiseNum < 0.345){return color(25,224,255);} //sea
+    else if (noiseNum < 0.348){return color(255,228,146);}
+    else if (noiseNum < 0.35){return color(250,213,99);}
+    else if (noiseNum < 0.355){return color(253,225,86);}
+    else if (noiseNum < 0.358){return color(250,226,126);}
+    else if (noiseNum < 0.36){return color(250,234,99);}
+    else if (noiseNum < 0.365){return color(250,237,129);} //beach
+    else if (noiseNum < 0.37){return color(220,250,126);}
+    else if (noiseNum < 0.38){return color(217,255,96);}
+    else if (noiseNum < 0.4){return color(169,237,58);}
+    else if (noiseNum < 0.42){return color(155,219,35);}
+    else if (noiseNum < 0.43){return color(136,209,30);}
+    else if (noiseNum < 0.44){return color(131,207,28);}
+    else if (noiseNum < 0.48){return color(101,185,32);}
+    else if (noiseNum < 0.49){return color(93,174,26);} 
+    else if (noiseNum < 0.5){return color(85,156,28);} 
+    else if (noiseNum < 0.53){return color(76,133,28);} 
+    else if (noiseNum < 0.56){return color(61,117,15);}
+    else if (noiseNum < 0.58){return color(55,96,22);} //grass
+    else if (noiseNum < 0.6){return color(86,61,2);}
+    else if (noiseNum < 0.62){return color(62,45,6);}
+    else if (noiseNum < 0.64){return color(80,64,26);}
+    else if (noiseNum < 0.66){return color(104,89,53);}
+    else if (noiseNum < 0.68){return color(110,100,75);}
+    else if (noiseNum < 0.7){ return color(121,115,102);} 
+    else if (noiseNum < 0.72){ return color(144,141,133);}
+    else if (noiseNum < 0.75){ return color(182,181,179);}
+    else if (noiseNum < 1.0){ return color(224,223,222);} //mountain-top
 }
 
 function setup() 
 {
     createCanvas(800,800);
     noStroke();
-    //colorMode(HSB);
+
     for(i = 0; i < width/tileSize;i++)
     {
         for(j = 0; j < height/tileSize;j++)
