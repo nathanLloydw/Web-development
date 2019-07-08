c0e57f45ecf343ea1d9d0cc6d3518ee31a8be6ed
Nathan Lloyd <43207434+nathanLloydw@users.noreply.github.com>
2019-07-08 17:17:35 +0100

    Update mapGeneration.js

--- JavaScript/mapGeneration.js
+++ JavaScript/mapGeneration.js
@@ -1,59 +1,63 @@
 let tileSize = 5;
-let scl = 0.018;
+let scl = 0.015;
+let IslandBoolean = false;
 
-var Map = [];
-let button;
-let IslandBoolean = true;
+let Map = [];
+let MapColors = ['#105D86','#0E6391','#116A9A','#1174A8','#1C75A4','#1879AD','#1D7DB0','#178AC8','#13ADDB','#0AB9EC','#19CBFF','#19E0FF',
+                 '#FFE492','#FAD563','#FDE156','#FAE27E','#FAEA63','#FAED81',
+                 '#DCFA7E','#D9FF60','#A9ED3A','#9BDB23','#88D11E','#83CF1C','#65B920','#5DAE1A','#559C1C','#4C851C','#3D750F','#376016',
+                 '#563D02','#3E2D06','#50401A','#685935','#6E644B','#797366','#908D85','#B6B5B3','#E0DFDE','#E0DFDE','#E0DFDE','#E0DFDE'];
+
 
 function getNoiseColor(noiseNum)
 { 
     switch(true)
     {
-        case(noiseNum < 0.25):return color(16,93,134);
-        case(noiseNum < 0.27):return color(14,99,145);
-        case(noiseNum < 0.28):return color(17,106,154); 
-        case(noiseNum < 0.29):return color(17,116,168); 
-        case(noiseNum < 0.31):return color(28,117,164);
-        case(noiseNum < 0.30):return color(24,121,173);
-        case(noiseNum < 0.315):return color(29,125,176);
-        case(noiseNum < 0.32):return color(23,138,200);
-        case(noiseNum < 0.325):return color(23,159,200);
-        case(noiseNum < 0.33):return color(19,173,219);
-        case(noiseNum < 0.335):return color(10,185,236);
-        case(noiseNum < 0.34):return color(25,203,255);
-        case(noiseNum < 0.345):return color(25,224,255); //sea
-        case(noiseNum < 0.348):return color(255,228,146);
-        case(noiseNum < 0.35):return color(250,213,99);
-        case(noiseNum < 0.355):return color(253,225,86);
-        case(noiseNum < 0.358):return color(250,226,126);
-        case(noiseNum < 0.36):return color(250,234,99);
-        case(noiseNum < 0.365):return color(250,237,129); //beach
-        case(noiseNum < 0.37):return color(220,250,126);
-        case(noiseNum < 0.38):return color(217,255,96);
-        case(noiseNum < 0.4):return color(169,237,58);
-        case(noiseNum < 0.42):return color(155,219,35);
-        case(noiseNum < 0.43):return color(136,209,30);
-        case(noiseNum < 0.44):return color(131,207,28);
-        case(noiseNum < 0.48):return color(101,185,32);
-        case(noiseNum < 0.49):return color(93,174,26); 
-        case(noiseNum < 0.5):return color(85,156,28); 
-        case(noiseNum < 0.53):return color(76,133,28); 
-        case(noiseNum < 0.56):return color(61,117,15);
-        case(noiseNum < 0.58):return color(55,96,22); //grass
-        case(noiseNum < 0.6):return color(86,61,2);
-        case(noiseNum < 0.62):return color(62,45,6);
-        case(noiseNum < 0.64):return color(80,64,26);
-        case(noiseNum < 0.66):return color(104,89,53);
-        case(noiseNum < 0.68):return color(110,100,75);
-        case(noiseNum < 0.7):return color(121,115,102); 
-        case(noiseNum < 0.72):return color(144,141,133);
-        case(noiseNum < 0.75):return color(182,181,179);
-        case(noiseNum < 1.0):return color(224,223,222); //mountain-top
+        case(noiseNum < 0.25): return 0;
+        case(noiseNum < 0.27): return 1;
+        case(noiseNum < 0.28): return 2; 
+        case(noiseNum < 0.29): return 3; 
+        case(noiseNum < 0.31): return 4;
+        case(noiseNum < 0.30): return 5;
+        case(noiseNum < 0.315):return 6;
+        case(noiseNum < 0.32): return 7;
+        case(noiseNum < 0.325):return 8;
+        case(noiseNum < 0.33): return 9;
+        case(noiseNum < 0.335):return 10;
+        case(noiseNum < 0.34): return 11;
+        case(noiseNum < 0.345):return 12; 
+        case(noiseNum < 0.348):return 13;
+        case(noiseNum < 0.35): return 14;
+        case(noiseNum < 0.355):return 15;
+        case(noiseNum < 0.358):return 16;
+        case(noiseNum < 0.36): return 17;
+        case(noiseNum < 0.365):return 18;
+        case(noiseNum < 0.37): return 19;
+        case(noiseNum < 0.38): return 20;
+        case(noiseNum < 0.4):  return 21;
+        case(noiseNum < 0.42): return 22;
+        case(noiseNum < 0.43): return 23;
+        case(noiseNum < 0.44): return 24;
+        case(noiseNum < 0.48): return 25;
+        case(noiseNum < 0.49): return 26;
+        case(noiseNum < 0.5):  return 27;
+        case(noiseNum < 0.53): return 28;
+        case(noiseNum < 0.56): return 29;
+        case(noiseNum < 0.58): return 30;
+        case(noiseNum < 0.6):  return 31;
+        case(noiseNum < 0.62): return 32;
+        case(noiseNum < 0.64): return 33;
+        case(noiseNum < 0.66): return 34;
+        case(noiseNum < 0.68): return 35;
+        case(noiseNum < 0.7):  return 36; 
+        case(noiseNum < 0.72): return 37;
+        case(noiseNum < 0.75): return 38;
+        case(noiseNum < 1.0):  return 39;
     }
-    return color(0,0,0);
+    return 0;
 }
 
-function drawColoredMap()
+function drawMap()
 {
     rows = width/tileSize;
     cols = height/tileSize;
@@ -61,34 +65,8 @@ function drawColoredMap()
     {
         for(j = 0; j < cols;j++)
         {
-              
-            if(IslandBoolean)
-            {
-                gradiantColor = get(i*tileSize,j*tileSize);
-                islandedColor = Map[i][j] - ((gradiantColor[0]/255));
-                c = getNoiseColor(islandedColor);
-            }
-            else
-            {
-                c = getNoiseColor(Map[i][j]);
-            }
-            
-            fill(c);
+            fill(color(MapColors[Map[i][j]]));
             rect(i * tileSize, j * tileSize,tileSize,tileSize);
-        }
-    }
-}
-
-function drawGrayMap()
-{
-    rows = width/tileSize;
-    cols = height/tileSize;
-    for(i = 0; i < rows;i++)
-    {
-        for(j = 0; j < cols;j++)
-        {          
-            fill(Map[i][j]*255);
-            rect(i * tileSize, j * tileSize,tileSize,tileSize);  
         }
     }
 }
@@ -102,7 +80,14 @@ function buildMap()
         Map[i] = [];
         for(j = 0; j < cols;j++)
         {          
-            Map[i][j] = (noise(i*scl,j*scl)); 
+            randomNoise = noise(i*scl,j*scl);
+
+            if(IslandBoolean)
+            {
+                gradiantColor = get(i*tileSize,j*tileSize);
+                randomNoise -= ((gradiantColor[0]/255));
+            }   
+            Map[i][j] = getNoiseColor(randomNoise); 
         }
     }
 }
@@ -135,6 +120,82 @@ function drawCircleGradiant(width,height,x,y)
     }
 }
 
+function getRandomFromArray(arr, n) 
+{
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) 
+    {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+}
+
+function spawnAI()
+{
+    rows = width/tileSize;
+    cols = height/tileSize;
+
+    Bunnys = [];
+    Foxes = [];
+    Berries = [];
+
+    for(i = 0; i < rows;i++)
+    {
+        for(j = 0; j < cols;j++)
+        {            
+            if(Map[i][j] == 22 || Map[i][j] == 23)
+            {
+              Bunnys.push(i+","+j);
+            }
+            else if(Map[i][j] == 29 || Map[i][j] == 28)
+            {
+              Foxes.push(i+","+j);
+            }
+            else if(Map[i][j] == 16 || Map[i][j] == 15 || Map[i][j] == 14 || Map[i][j] == 13 || Map[i][j] == 12)
+            {  
+              Berries.push(i+","+j);
+            }
+            
+        }
+    }
+
+    Bunnys = getRandomFromArray(Bunnys,58);
+    Foxes = getRandomFromArray(Foxes,15);
+    Berries = getRandomFromArray(Berries,21);
+
+    for(i in Bunnys)
+    {
+        bunny = Bunnys[i];
+        cords = bunny.split(",");
+        fill(color('#804000'));
+        circle(cords[0] * tileSize, cords[1] * tileSize,tileSize,tileSize); 
+    }
+
+    for(i in Foxes)
+    {
+        fox = Foxes[i];
+        cords = fox.split(",");
+        fill(color('#B20000'));
+        circle(cords[0] * tileSize, cords[1] * tileSize,tileSize,tileSize);  
+        fill(color('#ffffff'));
+        circle(cords[0] * tileSize, cords[1] * tileSize,tileSize/2,tileSize/2);  
+    }
+
+    for(i in Berries)
+    {
+        berry = Berries[i];
+        cords = berry.split(",");
+        fill(color('#0000FF'));
+        circle(cords[0] * tileSize, cords[1] * tileSize,tileSize,tileSize);  
+    }
+}
+
 function setup() 
 {
     width = 1600;
@@ -142,10 +203,14 @@ function setup()
     createCanvas(width,height);
     noStroke(); 
 
-    buildMap();
-    drawCircleGradiant(width,height,width/2,height/2);
     
-    drawColoredMap(); 
+    drawCircleGradiant(width,height,width/2,height/2);
+    buildMap();
+    drawMap(); 
+
+    spawnAI();
+
+
 }
 
 function draw() 
